10863e7da98d6b41e9b6c8cbc02d1d1d330965b6
Branden Wood <brandenek03912@hotmail.com>
2023-02-22 12:40:56 -0500

    Update required rando version

--- BlasphemousMultiworld/Main.cs
+++ BlasphemousMultiworld/Main.cs
@@ -5,7 +5,7 @@ namespace BlasphemousMultiworld
 {
     [BepInPlugin(MOD_ID, MOD_NAME, MOD_VERSION)]
     [BepInDependency("com.damocles.blasphemous.modding-api", "1.0.0")]
-    [BepInDependency("com.damocles.blasphemous.randomizer", "1.3.0")]
+    [BepInDependency("com.damocles.blasphemous.randomizer", "1.4.0")]
     [BepInProcess("Blasphemous.exe")]
     public class Main : BaseUnityPlugin
     {
