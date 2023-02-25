160d70b6b0aaa0af4da5a834dbb843d0a5210e24
Branden Wood <brandenek03912@hotmail.com>
2023-02-24 20:36:08 -0500

    Update version to 1.0.1

--- BlasphemousMultiworld/Main.cs
+++ BlasphemousMultiworld/Main.cs
@@ -4,14 +4,14 @@ using BlasphemousRandomizer;
 namespace BlasphemousMultiworld
 {
     [BepInPlugin(MOD_ID, MOD_NAME, MOD_VERSION)]
-    [BepInDependency("com.damocles.blasphemous.modding-api", "1.0.0")]
+    [BepInDependency("com.damocles.blasphemous.modding-api", "1.1.0")]
     [BepInDependency("com.damocles.blasphemous.randomizer", "1.4.0")]
     [BepInProcess("Blasphemous.exe")]
     public class Main : BaseUnityPlugin
     {
         public const string MOD_ID = "com.damocles.blasphemous.multiworld";
         public const string MOD_NAME = "Multiworld";
-        public const string MOD_VERSION = "1.0.0";
+        public const string MOD_VERSION = "1.0.1";
 
         public static Multiworld Multiworld;
         public static Randomizer Randomizer;
