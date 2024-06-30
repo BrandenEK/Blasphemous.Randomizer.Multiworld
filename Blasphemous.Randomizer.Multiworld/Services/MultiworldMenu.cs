0ed0a806f9090f76a59d5a3f8aa9cd49c3713b1e
unknown <brandenek03912@hotmail.com>
2024-06-30 15:04:06 -0400

    menu: add 3 options

--- Blasphemous.Randomizer.Multiworld/Services/MultiworldMenu.cs
+++ Blasphemous.Randomizer.Multiworld/Services/MultiworldMenu.cs
@@ -1,14 +1,32 @@
 ﻿using Blasphemous.Framework.Menus;
+using Blasphemous.Framework.Menus.Options;
 using UnityEngine;
 
 namespace Blasphemous.Randomizer.Multiworld.Services;
 
 public class MultiworldMenu : ModMenu
 {
+    private TextOption _server;
+    private TextOption _name;
+    private TextOption _password;
+
     protected override int Priority { get; } = int.MaxValue;
+
+    public override void OnFinish()
+    {
+        Main.Multiworld.Log($"Server {_server.CurrentValue} as {_name.CurrentValue} with pass {_password.CurrentValue}");
+    }
 
     protected override void CreateUI(Transform ui)
     {
-        Main.Multiworld.LogWarning("Created mw menu ui");
+        TextCreator text = new(this)
+        {
+            TextSize = 54,
+            LineSize = 400,
+        };
+
+        _server = text.CreateOption("server", ui, new Vector2(0, 100), "Server ip:", false, true, 64);
+        _name = text.CreateOption("name", ui, new Vector2(0, 0), "Player name:", false, true, 64);
+        _password = text.CreateOption("password", ui, new Vector2(0, -100), "Optional password:", false, true, 64);
     }
 }
