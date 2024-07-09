c50195ece8b4ab3954fb49d40af1d3074bd4cb7e
unknown <brandenek03912@hotmail.com>
2024-07-09 13:02:18 -0400

    console: change command to 'ap' and add hint command

--- Blasphemous.Randomizer.Multiworld/Services/MultiworldCommand.cs
+++ Blasphemous.Randomizer.Multiworld/Services/MultiworldCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Blasphemous.CheatConsole;
+using Gameplay.UI.Widgets;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,7 +8,7 @@ namespace Blasphemous.Randomizer.Multiworld.Services
 {
     public class MultiworldCommand : ModCommand
     {
-        protected override string CommandName => "multiworld";
+        protected override string CommandName => "ap";
 
         protected override bool AllowUppercase => true;
 
@@ -19,22 +20,24 @@ namespace Blasphemous.Randomizer.Multiworld.Services
                 { "status", Status },
                 //{ "deathlink", Deathlink },
                 { "say", Say },
+                { "hint", Hint },
             };
         }
 
         public void HackWriteToConsole(string message)
         {
-            Write(message);
+            ConsoleWidget.Write(message);
         }
 
         private void Help(string[] parameters)
         {
             if (!ValidateParameterList(parameters, 0)) return;
 
-            Write("Available MULTIWORLD commands:");
-            Write("multiworld status: Display connection status");
+            Write("Available AP commands:");
+            Write("ap status: Display connection status");
             //Write("multiworld deathlink: Toggles deathlink on/off");
-            Write("multiworld say COMMAND: Sends a text message or command to the server");
+            Write("ap say COMMAND: Sends a text message or command to the server");
+            Write("ap hint ITEM: Hints for your specified item in the multiworld");
         }
 
         private void Status(string[] parameters)
@@ -83,6 +86,37 @@ namespace Blasphemous.Randomizer.Multiworld.Services
                 output.AppendFormat(" {0}", parameters[i]);
             }
             Main.Multiworld.APManager.SendMessage(output.ToString());
+        }
+
+        private void Hint(string[] parameters)
+        {
+            // Too few parameters
+            if (parameters.Length < 1)
+            {
+                Write("This command requires at least one parameter.  You passed " + parameters.Length);
+                return;
+            }
+
+            // Not connected to multiworld
+            if (!Main.Multiworld.APManager.Connected)
+            {
+                Write("Not connected to any server!");
+                return;
+            }
+
+            string item = string.Join(" ", parameters);
+            Main.Multiworld.APManager.SendHint(item);
+        }
+
+        private ConsoleWidget x_console;
+        private ConsoleWidget ConsoleWidget
+        {
+            get
+            {
+                if (x_console == null)
+                    x_console = UnityEngine.Object.FindObjectOfType<ConsoleWidget>();
+                return x_console;
+            }
         }
     }
 }
