623ac91295159208ec9729880d5863746deaa50d
unknown <brandenek03912@hotmail.com>
2024-07-12 10:37:48 -0400

    core: load items on connect

--- Blasphemous.Randomizer.Multiworld/LocationScouter.cs
+++ Blasphemous.Randomizer.Multiworld/LocationScouter.cs
@@ -1,4 +1,10 @@
-﻿
+﻿using Archipelago.MultiClient.Net;
+using Blasphemous.Randomizer.Multiworld.AP;
+using Blasphemous.Randomizer.Multiworld.Models;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Blasphemous.Randomizer.Multiworld;
 
 /// <summary>
@@ -6,4 +12,46 @@ namespace Blasphemous.Randomizer.Multiworld;
 /// </summary>
 public class LocationScouter
 {
+    private readonly Dictionary<string, MultiworldItem> _multiworldItems = new();
+    private readonly List<KeyValuePair<string, long>> _idMapping = new();
+
+    public LocationScouter()
+    {
+        Main.Multiworld.APManager.OnConnect += OnConnect;
+    }
+
+    private void OnConnect(LoginResult login)
+    {
+        if (login is not LoginSuccessful success)
+            return;
+
+        _multiworldItems.Clear();
+        _idMapping.Clear();
+
+        // Get location list from slot data
+        ArchipelagoLocation[] locations = ((JArray)success.SlotData["locations"]).ToObject<ArchipelagoLocation[]>();
+
+        foreach (ArchipelagoLocation location in locations)
+        {
+            // Add id mapping
+            _idMapping.Add(new KeyValuePair<string, long>(location.id, location.ap_id));
+
+            MultiworldItem item = location.player_name == Main.Multiworld.ClientSettings.Name // Probably wont work
+                ? GetSelfItem(location)
+                : GetOtherItem(location);
+
+            // Add item to mappedItems
+            _multiworldItems.Add(location.id, item);
+        }
+    }
+
+    private MultiworldItem GetSelfItem(ArchipelagoLocation location)
+    {
+        return new MultiworldSelfItem(location.id, Main.Randomizer.data.items.Values.First(x => x.name == location.name));
+    }
+
+    private MultiworldItem GetOtherItem(ArchipelagoLocation location)
+    {
+        return new MultiworldOtherItem(location.id, location.name, location.player_name, (MultiworldOtherItem.ItemType)location.type);
+    }
 }
