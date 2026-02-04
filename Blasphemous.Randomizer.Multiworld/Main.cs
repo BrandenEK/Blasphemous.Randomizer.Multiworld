3c523afc496890b30fe7dac2e7acb173b631fd6c
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-04 12:20:14 -0500

    core: upgrade dependencies

--- Blasphemous.Randomizer.Multiworld/Main.cs
+++ Blasphemous.Randomizer.Multiworld/Main.cs
@@ -6,8 +6,8 @@ namespace Blasphemous.Randomizer.Multiworld;
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
 [BepInDependency("Blasphemous.ModdingAPI", "3.0.0")]
 [BepInDependency("Blasphemous.Randomizer", "3.2.0")]
-[BepInDependency("Blasphemous.CheatConsole", "1.0.1")]
-[BepInDependency("Blasphemous.Framework.Menus", "0.4.0")]
+[BepInDependency("Blasphemous.CheatConsole", "1.1.0")]
+[BepInDependency("Blasphemous.Framework.Menus", "0.6.0")]
 internal class Main : BaseUnityPlugin
 {
     public static Multiworld Multiworld { get; private set; }
